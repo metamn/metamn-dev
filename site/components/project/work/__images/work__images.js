1d40469e8ea6063e16b0621dc2b03d906695d453
cs <bartus.csongor@gmail.com>
2015-04-02 00:15:47 +0300

    Navigation arrow behavior fixed

--- site/components/project/work/__images/work__images.js
+++ site/components/project/work/__images/work__images.js
@@ -7,17 +7,17 @@ var pos = 0;
 
 function setTransform() {
   for (var i = 0; i < itemCount; i++ ) {
-    items[i].style['transform'] = 'translateX(' + ((-i + pos) * items[0].offsetWidth) + 'px)';
+    items[i].style['transform'] = 'translateX(' + ((i + pos) * items[0].offsetWidth) + 'px)';
   }
 }
 
 function prev() {
-  pos = Math.max(pos - 1, 0);
+  pos = Math.max(pos - 1, -(itemCount - 1));
   setTransform();
 }
 
 function next() {
-  pos = Math.min(pos + 1, itemCount - 1);
+  pos = Math.min(pos + 1, 0);
   setTransform();
 }
 
