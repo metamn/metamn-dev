4ae1253cd62a3108e7706928b99bdf5b64e21a92
cs <bartus.csongor@gmail.com>
2015-05-13 12:10:05 +0300

    Styleguide transform click fixed for Webkit

--- styleguide/components/pages/framework/behavior/transition/transition.js
+++ styleguide/components/pages/framework/behavior/transition/transition.js
@@ -10,14 +10,22 @@ var transitionExample = function(examples) {
     if (transform) {
       var minus = transform.indexOf('50');
       if (minus !== -1) {
-        this.style['transform'] = 'translateX(0px)';
+        _transform(this, '0px');
       } else {
-        this.style['transform'] = 'translateX(50px)';
+        _transform(this, '50px');
       }
     } else {
-      this.style['transform'] = 'translateX(50px)';
+      _transform(this, '50px');
     }
+  }
 
+  function _transform(item, value) {
+    item.style.webkitTransform = 'translate(' + value + ', 0)' + 'translateZ(0)';
+
+    item.style.MozTransform =
+    item.style.msTransform =
+    item.style.OTransform =
+    item.style.transform = 'translateX(' + value + ')';
   }
 }
 
