eb41d94619bb34ca71cc2e85b40916177837b817
metamn <bartus.csongor@gmail.com>
2016-04-12 11:49:03 +0300

    Ready to add a gradient background

--- site/components/pages/home/__intro/home__intro.js
+++ site/components/pages/home/__intro/home__intro.js
@@ -6,7 +6,67 @@ var introAnimation = function(slidesID) {
     introAnimationDisplayYear(slides[i], i + 1);
   }
 
-  introAnimationAddGrid(slides[1], 2);
+  introAnimationAddBackground(slides[1], 2);
+}
+
+
+
+function introAnimationAddBackground(slide, index) {
+  var position = slide.getBoundingClientRect();
+  var w = position.width - 128;
+  var h = position.height - 128;
+
+  var s = document.querySelector('.home__intro--animation-' + index);
+
+  var d = document.createElement('div');
+  d.className = 'background';
+  d.style.width = w + 'px';
+  d.style.height = h + 'px';
+  s.appendChild(d);
+}
+
+
+function introAnimationDisplayYear(slide, activeSlideIndex) {
+  var position = slide.getBoundingClientRect();
+
+  var year = slide.dataset.year;
+  if (!year) return;
+
+  var w = position.width - 32;
+  var h = position.height - 32;
+
+  var d = document.createElement('div');
+  d.className = 'home__intro--animation-' + activeSlideIndex;
+
+  var s1 = document.createElement('span');
+  s1.innerHTML = year[0];
+  s1.className = 'letter letter--1';
+  s1.style.top = '16px';
+  s1.style.left = '16px';
+  d.appendChild(s1);
+
+  var s2 = document.createElement('span');
+  s2.innerHTML = year[1];
+  s2.className = 'letter letter--2';
+  s2.style.top = '16px';
+  s2.style.left = w + 'px';
+  d.appendChild(s2);
+
+  var s3 = document.createElement('span');
+  s3.innerHTML = year[2];
+  s3.className = 'letter letter--3';
+  s3.style.top = h + 'px';
+  s3.style.left = '16px';
+  d.appendChild(s3);
+
+  var s4 = document.createElement('span');
+  s4.innerHTML = year[3];
+  s4.className = 'letter letter--4';
+  s4.style.top = h + 'px';
+  s4.style.left = w + 'px';
+  d.appendChild(s4);
+
+  slide.appendChild(d);
 }
 
 
@@ -32,50 +92,6 @@ function introAnimationAddGrid(slide, index) {
   s.appendChild(d);
 }
 
-
-
-function introAnimationDisplayYear(slide, activeSlideIndex) {
-  var position = slide.getBoundingClientRect();
-
-  var year = slide.dataset.year;
-  if (!year) return;
-
-  var w = position.width - (position.width / 30);
-  var h = position.height - (position.height / 30);
-
-  var d = document.createElement('div');
-  d.className = 'home__intro--animation-' + activeSlideIndex;
-
-  var s1 = document.createElement('span');
-  s1.innerHTML = year[0];
-  s1.className = 'letter letter--1';
-  s1.style.top = '0';
-  s1.style.left = '0';
-  d.appendChild(s1);
-
-  var s2 = document.createElement('span');
-  s2.innerHTML = year[1];
-  s2.className = 'letter letter--2';
-  s2.style.top = '0';
-  s2.style.left = w + 'px';
-  d.appendChild(s2);
-
-  var s3 = document.createElement('span');
-  s3.innerHTML = year[2];
-  s3.className = 'letter letter--3';
-  s3.style.top = h + 'px';
-  s3.style.left = '0';
-  d.appendChild(s3);
-
-  var s4 = document.createElement('span');
-  s4.innerHTML = year[3];
-  s4.className = 'letter letter--4';
-  s4.style.top = h + 'px';
-  s4.style.left = w + 'px';
-  d.appendChild(s4);
-
-  slide.appendChild(d);
-}
 
 
 
